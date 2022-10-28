a149442bf2036e4c8c408c8fd5058dbb729cd534
dgiyevskiy <gievsky.dmitry@gmail.com>
2022-10-28 16:49:05 +0300

    add picture ready component

---
+++ MvcProject.WebNewApp/ViewComponents/PictureReadyViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcProject.Bll.ViewModels.Common;
+
+namespace MvcProject.WebNewApp.ViewComponents
+{
+    public class PictureReadyViewComponent : ViewComponent
+    {
+        private const string DefaultPicturePath = "/Pictures/Default/default-1.jpg";
+
+        public IViewComponentResult Invoke(string picturePath)
+        {
+            return View("Picture", new PictureViewModel { Path = string.IsNullOrEmpty(picturePath) ? DefaultPicturePath : picturePath });
+        }
+    }
+}
