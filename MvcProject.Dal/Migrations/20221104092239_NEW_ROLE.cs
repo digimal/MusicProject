ba68a8eed409226ec60082553dae9f9dcc5c6c48
digimal <gievsky.dmitry@gmail.com>
2022-11-10 02:13:47 +0200

    Add role

---
+++ MvcProject.Dal/Migrations/20221104092239_NEW_ROLE.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcProject.Dal.Migrations
+{
+    public partial class NEW_ROLE : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT [dbo].AspNetRoles (Name, NormalizedName) VALUES ('test','TEST')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
